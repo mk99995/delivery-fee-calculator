a5f21bb50243202b548d11c880b68cf5052a6e82
mk99995 <mejkorte@gmail.com>
2023-02-04 16:03:06 +0200

    small changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,17 @@ import "./App.scss";
 function App() {
   const [count, setCount] = useState(0);
 
+  let calculateFee = (e: any) => {
+    e.preventDefault();
+    setCount(count + 1);
+  };
+
   return (
-    <form className="App">
+    <form className="App" onSubmit={calculateFee}>
       <input type="text" />
       <input type="text" />
       <input type="text" />
-      <input type="date" />
+      <input type="datetime-local" />
       <input type="submit" value="Calculate delivery price" />
       <p>{count}</p>
     </form>
